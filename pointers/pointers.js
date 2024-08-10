63858766692e0b851c8b0fa6e2194aaa7fb0c4e9
mateooo11 <pitu.bprzz@gmail.com>
2024-08-10 16:00:11 +0200

    Create pointers.js

---
+++ pointers/pointers.js
@@ -0,0 +1,19 @@
+/* Default cursor */
+body, * {
+    cursor: url('handpointing.svg'), auto;
+  }
+  
+  /* Clickable elements */
+  a, button, [role="button"], input[type="submit"], input[type="button"] {
+    cursor: url('handpointing.svg'), pointer;
+  }
+  
+  /* Text input areas */
+  input[type="text"], textarea, [contenteditable="true"] {
+    cursor: url('textcursor.svg'), text;
+  }
+  
+  /* Elements with title attribute or help icons */
+  [title], .help-icon {
+    cursor: url('help.svg'), help;
+  }
